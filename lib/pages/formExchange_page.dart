132096246cf6337330ef7c37f8acdd68447be77e
hendra0123 <nerstuff24@gmail.com>
2025-06-18 23:39:45 +0800

    testing github action

--- lib/pages/formExchange_page.dart
+++ lib/pages/formExchange_page.dart
@@ -5,13 +5,11 @@ import 'package:smartbin/utils/exchange_validator.dart'; // ✅ Tambahkan import
 class FormExchangePage extends StatefulWidget {
   final int currentPoints;
   final int requiredPoints;
-  final int nada;
 
   const FormExchangePage({
     super.key,
     this.currentPoints = 120,
     this.requiredPoints = 100,
-    this.nada = 0,
   });
 
   @override
